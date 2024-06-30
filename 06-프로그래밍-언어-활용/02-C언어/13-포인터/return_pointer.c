7d2cdc5a59d9cd50547970cbf12370afc1820122
krozv <krozv.code@gmail.com>
2024-06-30 18:35:59 +0900

    upload 13-포인터

---
+++ 06-프로그래밍-언어-활용/02-C언어/13-포인터/return_pointer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+char n[6];
+char *soojebi(){
+    strcpy(n, "Hello");
+    return n;
+}
+int main() {
+    char *p = soojebi();
+    printf("%s\n", p);
+    return 0;
+}
+// 출력
+// Hello
---
+++ 06-프로그래밍-언어-활용/02-C언어/13-포인터/struncture_pointer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+struct Student{
+    char gender;
+    int age;
+};
+int main(){
+    struct Student s = {'F', 21};
+    struct Student *p = &s;
+    printf("%c %d\n", s.gender, s.age);
+    printf("%c %d\n", (&s)->gender, (&s)->age);
+    printf("%c %d\n", p->gender, p->age);
+    printf("%c %d\n", (*p).gender, (*p).age);
+    printf("%c %d\n", p[0].gender, p[0].age);
+    return 0;
+}
+// 출력
+// F 21
+// F 21
+// F 21
+// F 21
+// F 21
